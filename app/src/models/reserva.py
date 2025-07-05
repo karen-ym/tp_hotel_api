4ee1c62569ab436e2dfab2abfc4796e692bfc6c1
David vasquez <vasquezdavid325@gmail.com>
2025-07-05 09:38:26 -0300

    Se agrego modelo reserva

---
+++ app/src/models/reserva.py
@@ -0,0 +1,12 @@
+from dependencias import db
+
+class Reserva(db.Model):
+    __tablename__ = "reservas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    habitacion_id = db.Column(db.Integer, db.ForeignKey("habitaciones.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    fecha = db.Column(db.Date, nullable=False)
+
+    habitacion = db.relationship("Habitacion", back_populates="reservas")
+    user = db.relationship("User", back_populates="reservas")
