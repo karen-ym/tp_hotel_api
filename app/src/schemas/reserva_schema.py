c628c7646f01daac01b0e4fb78d3b6ae9b2b05c3
David vasquez <vasquezdavid325@gmail.com>
2025-07-05 10:37:41 -0300

    Incorporacion de Schema

---
+++ app/src/schemas/reserva_schema.py
@@ -0,0 +1,13 @@
+from marshmallow import Schema, fields, validates, ValidationError
+from dependencias import db, ma
+
+class ReservaSchema(Schema):
+    id = fields.Int(dump_only=True)
+    habitacion_id = fields.Int(required=True)
+    usuario_id = fields.Int(required=True)
+    fecha = fields.Date(required=True)
+
+    @validates("fecha")
+    def validar_fecha(self, value):
+        if not value:
+            raise ValidationError("La fecha es requerida")
