cf1d1ab078c9a6d40913eb1e349a9748e7dc8f49
karen-ym <karenymorel@gmail.com>
2025-07-05 01:44:01 -0300

    Modificaciones en app.py para soporte de postgre

--- app/app.py
+++ app/app.py
@@ -1,21 +1,31 @@
 from flask import Flask
-from src.routes.auth_bp import auth_bp 
 from dependencias import db, ma
-from src.routes.habitaciones_bp import habitaciones_bp
 
-app = Flask(__name__)
+def create_app():
 
-db.init_app(app)
-ma.init_app(app)
+  app = Flask(__name__)
+  app.config["SECRET_KEY"] = "pass1234"  # Clave secreta para JWT (debería estar en una variable de entorno? chequear despues)
 
-app.config["SECRET_KEY"] = "pass1234"  # Clave secreta para JWT (debería estar en una variable de entorno? chequear despues)
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://nombreUsuario:clave@localhost:5432/hotel_api_db' # -> COMPLETAR CON LOS DATOS DE LA DB DE USTDS (hotel_api_db es el nombre de la db, llamenla así.)
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.register_blueprint(auth_bp) # Registrar el blueprint
-app.register_blueprint(habitaciones_bp)
+  from src.models.user import User
 
-@app.route("/")
-def prueba():
-  return "Hola!"
+  db.init_app(app)
+  ma.init_app(app)
+
+  from src.routes.auth_bp import auth_bp
+  from src.routes.habitaciones_bp import habitaciones_bp
+
+  app.register_blueprint(auth_bp) # Registrar el blueprint
+  app.register_blueprint(habitaciones_bp)
+
+  return app
 
 if __name__ == "__main__":
+  app = create_app()
+
+  with app.app_context():
+    db.create_all()  # Crea las tablas en la base de datos (SI no existen)
+
   app.run(debug=True, host="0.0.0.0", port=5000) # En el .exe -> urlbase: http://localhost:5000
--- app/src/models/user.py
+++ app/src/models/user.py
@@ -1,4 +1,4 @@
-from app.app import db
+from dependencias import db
 
 class User(db.Model):
   __tablename__ = 'users'
