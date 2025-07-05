ddda37a347af58ab0b047b79fc07241315fed36d
karen-ym <karenymorel@gmail.com>
2025-07-04 23:35:04 -0300

    Agrego modelo User

---
+++ app/src/models/user.py
@@ -0,0 +1,10 @@
+from app.app import db
+
+class User(db.Model):
+  __tablename__ = 'users'
+  
+  # Le pongo los nombres en inglés porque la consigna pide que sean =! a los que se esperan en los endpoints (usuario, clave, categoria)
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), unique=True, nullable=False)
+  password = db.Column(db.String(255), nullable=False) # chequear si entra el hash aca
+  role = db.Column(db.String(50), nullable=False)
