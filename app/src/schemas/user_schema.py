06ce988f42fa58baf35c4ba5d71d68f45de7e269
karen-ym <karenymorel@gmail.com>
2025-07-05 07:47:21 -0300

    Cargar user schema.

---
+++ app/src/schemas/user_schema.py
@@ -0,0 +1,29 @@
+from dependencias import ma
+from marshmallow import fields, validate
+
+class UserSchema(ma.Schema):
+  # Los nombres coinciden con lo que espera el endpoint del .exe (endpoint != atributo en db)
+
+  usuario = fields.Str(
+    required=True,
+    error_messages={"required": "El nombre de usuario es obligatorio."}
+  )
+  categoria = fields.Str(
+    required=True,
+    validate=validate.OneOf(["Cliente", "Empleado"], error="La categoría debe ser 'Cliente' o 'Empleado'."),
+    error_messages={"required": "La categoría es obligatoria."}
+  )
+  clave1 = fields.Str(
+    required=True,
+    error_messages={"required": "La clave es obligatoria."}
+  )
+  clave2 = fields.Str(
+    required=True,
+    error_messages={"required": "La confirmación de la clave es obligatoria."}
+  )
+
+  class Meta:
+    fields = ("usuario", "categoria", "clave1", "clave2")
+
+# Instancia del schema 
+user_schema = UserSchema()
